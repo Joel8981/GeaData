e0d274647eada0d0d04dee7fab192eb2ad617d2c
Joel8981 <ajoel9632@gmail.com>
2025-11-11 21:33:56 -0300

    Opcion 2 del menu agregada, y funcion modo_lectura
    En este bloque hemos mejorado ubicando un modo lectura para la opcion 1 y 2

--- GeaData.py
+++ GeaData.py
@@ -45,39 +45,58 @@ def mostrar_opciones():
    -> 3. Ordenar por (nombre, poblacion o superficie) ⌨️
    -> 4. Mostrar estadisticas 🧮
    -> 5. Salir 🏃‍♂️‍➡️""")
+
+#Creamos esta opcion para el modo lectura de la opcion 1 y 2
+def modo_lectura_csv(CSV, opcion, buscar):
     
-def buscar_pais(CSV):
+    #Abrimos el archivo en modo lectura para buscar
+    with open(CSV, "r") as archivo_Csv:
+    
+        # Creamos el objeto DictReader.
+        #Transformamos cada columna del archivo csv en claves
+        lector_diccionario = csv.DictReader(archivo_Csv)
+        
+        #
+        #Con un bucle recorremos para buscar el pais del usuario
+        for filas in lector_diccionario:
+            
+            #Defini en variables cada clave del diccionario
+            nombre_pais = filas["nombre"]
+            poblacion = filas["poblacion"]
+            superficie = filas["superficie"]
+            continente = filas["continente"]
+            
+            #Condicional para la opcion 1 (buscar y mostrar el pais buscante)
+            if opcion == 1:
+                if nombre_pais == buscar:
+                    print("-------------------------------------------------------")
+                    print(f" ->> Pais: {nombre_pais} || Poblacion: {poblacion} || Superficie: {superficie} km² || Continente: {continente} ")
+                    print("-----------------------------------------------------------")
+                    break
+            
+            #Este para la opcion 2 (mostrar todos lo paises con su informacion )
+            elif opcion == 2:
+                print(f" - Pais: {nombre_pais} || Poblacion: {poblacion} || Superficie: {superficie} km² || Continente: {continente} ")
+                continue
+        
+def buscar_pais(CSV, opcion):
     
     print(" - Buscar Pais 🔎🚩")
     
     #Le pedimos el pais a buscar
     buscar = input("Ingresa el nombre del pais a buscar: ").capitalize()
     
-    try:
-        #Abrimos el archivo en modo lectura para buscar
-        with open(CSV, "r") as archivo_Csv:
-        
-            # Creamos el objeto DictReader.
-            #Transformamos cada columna del archivo csv en claves
-            lector_diccionario = csv.DictReader(archivo_Csv)
-            
-            #
-            #Con un bucle recorremos para buscar el pais del usuario
-            for filas in lector_diccionario:
-                
-                #Defini en variables cada
-                nombre_pais = filas["nombre"]
-                poblacion = filas["poblacion"]
-                superficie = filas["superficie"]
-                continente = filas["continente"]
-                if nombre_pais == buscar:
-                    print("-------------------------------------------------------")
-                    print(f" ->> Pais: {nombre_pais} || Poblacion: {poblacion} || Superficie: {superficie}km² || Continente: {continente} ")
-                    print("-----------------------------------------------------------")
-                    break
-    except:
-        print("Error, El dato ingresado no corresponde a un pais")
-                
+    modo_lectura_csv(CSV, opcion, buscar)
+    
+    
+#Funcion para filtrar paises
+def filtrar_paises(CSV, opc):
+    
+    #enviamos al modo lectura(funcion que se reutilizara ya que casi cumple la misma funcion que la opcion 1)
+    print(" - Filtrar paises - ")
+    modo_lectura_csv(CSV, opc, None)
+    
+         
 #Creamos el menu
 def main(CSV):
     
@@ -95,11 +114,16 @@ def main(CSV):
             
             #Condicional para llamar a las funciones segun lo que quiera el usuario
             if opc == 1:
-                buscar_pais(CSV)
+                buscar_pais(CSV, opc)
+            elif opc == 2:
+                filtrar_paises(CSV, opc)
+                
             
             break
         except:
             print("❌ Error, el dato ingresado no corresponde a una opcion")
+            print("Cerrando programa..")
+            break
 
 #  -------- INICIO DEL PROGRAMA PRINCIPAL ------------------
 #Creamos el nombre del archivo csv con su extension
